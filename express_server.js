289001b608cab5db758b4bd6c9227e49cedeb72e
Shilpa <shilpa.pulipati24@gmail.com>
2023-05-02 23:18:26 -0400

    adding urls_show.ejs in express_server.js file

--- express_server.js
+++ express_server.js
@@ -22,3 +22,11 @@ app.listen(PORT, () => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+});
