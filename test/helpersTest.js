15dcef9292540900fd7a56fa4e9f9443d14a7362
Shilpa <shilpa.pulipati24@gmail.com>
2023-05-17 15:24:43 -0400

    added helperTest.js file

---
+++ test/helpersTest.js
@@ -0,0 +1,28 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    // Write your assert statement here
+  });
+  it("should return undefined for non-existent email", function () {
+    const user = getUserByEmail("nonexistent@example.com", testUsers);
+    assert.equal(user, undefined);
+  });
+});
